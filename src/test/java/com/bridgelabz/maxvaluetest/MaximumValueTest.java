01ab731b36d593c104349de8cdbce232aac679e2
Amrut <amrutpanda7789@gmail.com>
2020-06-21 04:09:01 +0530

    ADDED:Maximum integer value at first position

---
+++ src/test/java/com/bridgelabz/maxvaluetest/MaximumValueTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.maxvaluetest;
+
+import com.bridgelabz.maxvalue.MaximumValue;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MaximumValueTest {
+    MaximumValue maximumValue;
+    @Before
+    public void createObjectOf_MaximumValue(){
+
+        maximumValue = new MaximumValue();
+    }
+
+    @Test
+    public void givenMaxIntegerNumber_AtFirstPosition_ShouldReturnSameNumber(){
+        Integer maximum = maximumValue.testMaximum(78, 52, 43);
+        Assert.assertEquals((Integer) 78, maximum);
+    }
+}
